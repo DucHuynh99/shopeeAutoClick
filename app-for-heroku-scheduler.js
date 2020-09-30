e391b86fd215ac45043ac89229d10c03c1acf6ea
Huynh Huu Duc <1712354@student.hcmus.edu.vn>
2020-09-30 14:17:22 +0700

    Add "View recent activity" feature

--- app-for-heroku-scheduler.js
+++ app-for-heroku-scheduler.js
@@ -1,44 +1,42 @@
 // @ts-check
-const TIME_INTERVAL = 15 * 60 * 1000;
-const TIME_TO_LOAD = 60 * 1000;
+const TIME_TO_LOAD = 10 * 1000;
 
 const { cookies, homeUrl, coinUrl, luckyUrl } = require('./shopeeData');
 const puppeteer = require('puppeteer');
 const moment = require('moment-timezone');
 
-(async () => {
+(async() => {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ["--no-sandbox"]
+        args: ["--no-sandbox", "--start-maximized"]
     });
     const page = await browser.newPage();
     await page.setCookie(...cookies);
+    await page.setViewport({ width: 1440, height: 900 });
     const now = moment.tz('Asia/Ho_Chi_Minh').hour();
     switch (now) {
-        case 9: case 10: case 11: case 12: case 15: case 18: case 21:
-            console.log(`Lúc ${now} giờ: Quà tặng Shopee!`);
-            try {
-                await page.goto(luckyUrl);
-            } catch (error) {
-                console.log('URL không hợp lệ!');
-            }
-            await page.waitFor(TIME_TO_LOAD);
-            try {
-                await page.frames()[1].click('#clickArea');
-            } catch (error) {
-                console.log('Bạn đã nhận quà rồi!');
-            }
-            await page.waitFor(TIME_INTERVAL - TIME_TO_LOAD);
-            break;
-        default:
+        case 0:
             console.log(`Lúc ${now} giờ: Săn xu mỗi ngày!`);
             await page.goto(coinUrl);
             await page.waitFor(TIME_TO_LOAD);
-            try {
-                await page.click('button._1Puh5H');
-            } catch (error) {
-                console.log('Bạn đã nhận xu rồi!');
-            }
-            await page.waitFor(TIME_INTERVAL - TIME_TO_LOAD);
+            await page.click('button._1Puh5H');
+            await page.waitFor(2000);
+            await page.screenshot({ path: './public/images/screenshot.png', fullPage: true });
+            break;
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 15:
+        case 18:
+        case 21:
+            console.log(`Lúc ${now} giờ: Quà tặng Shopee!`);
+            await page.goto(luckyUrl);
+            await page.waitFor(TIME_TO_LOAD);
+            await page.frames()[1].click('#clickArea');
+            await page.waitFor(2000);
+            await page.screenshot({ path: './public/images/screenshot.png', fullPage: true });
+            break;
     }
+    await browser.close();
 })();
--- app.js
+++ app.js
@@ -1,72 +1,15 @@
 // @ts-check
-const TIME_INTERVAL = 15 * 60 * 1000;
-const TIME_TO_LOAD = 60 * 1000;
-
-const { cookies, homeUrl, coinUrl, luckyUrl } = require('./shopeeData');
-const puppeteer = require('puppeteer');
-const moment = require('moment-timezone');
+const port = process.env.PORT || 8080;
+const path = require('path');
 const express = require('express');
-const port = process.env.PORT || 80;
 const app = express();
 
-app.get('/', function (req, res) { res.send('Hello'); });
+app.use(express.static(path.join(__dirname, "public")));
 
-// (async () => {
-//     var visited = Array(24).fill(0);
-//     const browser = await puppeteer.launch({
-//         headless: true,
-//         args: ["--no-sandbox"]
-//     });
-//     const page = await browser.newPage();
-//     await page.setCookie(...cookies);
-//     await page.goto(homeUrl);
-//     await page.waitFor(TIME_TO_LOAD);
-//     console.log('Khởi động thành công!');
-//     while (true) {
-//         const now = moment.tz('Asia/Ho_Chi_Minh').hour();
-//         switch (now) {
-//             case 9: case 10: case 11: case 12: case 15: case 18: case 21:
-//                 if (visited[now] == 0) {
-//                     console.log(`Lúc ${now} giờ: Quà tặng Shopee!`);
-//                     visited[now] = 1;
-//                     try {
-//                         await page.goto(luckyUrl);
-//                     } catch (error) {
-//                         console.log('URL không hợp lệ!');
-//                     }
-//                     await page.waitFor(TIME_TO_LOAD);
-//                     try {
-//                         await page.frames()[1].click('#clickArea');
-//                     } catch (error) {
-//                         console.log('Bạn đã nhận quà rồi!');
-//                     }
-//                     await page.waitFor(TIME_INTERVAL - TIME_TO_LOAD);
-//                 }
-//                 else {
-//                     console.log(`Lúc ${now} giờ: Chờ ${TIME_INTERVAL / 60 / 1000} phút`);
-//                     await page.waitFor(TIME_INTERVAL);
-//                 }
-//                 break;
-//             default:
-//                 if (now == 0 && visited[21] == 1)
-//                     visited.fill(0);
-//                 if (visited[0] == 0) {
-//                     console.log(`Lúc ${now} giờ: Săn xu mỗi ngày!`);
-//                     visited[0] = 1;
-//                     await page.goto(coinUrl);
-//                     await page.waitFor(TIME_TO_LOAD);
-//                     try {
-//                         await page.click('button._1Puh5H');
-//                     } catch (error) {
-//                         console.log('Bạn đã nhận xu rồi!');
-//                     }
-//                     await page.waitFor(TIME_INTERVAL - TIME_TO_LOAD);
-//                 } else {
-//                     console.log(`Lúc ${now} giờ: Chờ ${TIME_INTERVAL / 60 / 1000} phút`);
-//                     await page.waitFor(TIME_INTERVAL);
-//                 }
-//         }
-//     };
-// })();
+app.get('/', function (req, res) {
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
